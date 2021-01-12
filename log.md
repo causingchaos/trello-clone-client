38b84108ad087e69cb69271e9ec69f759cb19223
causingchaos <Justin.Berry@albertahealthservices.ca>
2021-01-12 16:30:16 -0700

    code before modification

--- log.md
+++ log.md
@@ -2,3 +2,6 @@ Install log
 
 $npx @vue/cli create client
 $npm install -g @vue/cli
+$npm install feathers-vuex --save
+$npm install @feathersjs/feathers @feathersjs/socketio-client --save
+$npm install socket.io-client@2.4.0   * Note feathersjs/socket-io client doesn't work with v3xx
--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    > App
+    </v-app-bar>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
-</style>
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
